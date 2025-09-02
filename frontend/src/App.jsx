3413dfca39464cc019c76f855dc9e9b28c34ab73
karimabraham0822 <karim.abraham0622@gmail.com>
2025-09-02 10:01:43 +0300

    Added Some Improvements To Cope With The Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,15 @@ import {
   useLocation,
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import PosterSlider from "./components/PosterSlider";
 import FloatingIcons from "./components/FloatingIcons";
+import Classroom from "./components/Classroom";
+import Courses from "./components/Courses";
+import Community from "./components/Community";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
 import Signup from "./components/Signup";
+import Footer from "./components/Footer";
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -15,8 +22,16 @@ function AnimatedRoutes() {
     <AnimatePresence mode="wait">
       <FloatingIcons />
       <Routes location={location} key={location.pathname}>
-        <Route path="/signup" element={<Signup />} />
+        {/* Show slider on homepage */}
+        <Route path="/App.jsx" element={<PosterSlider />} />
+        <Route path="/Classroom" element={<Classroom />} />
+        <Route path="/Courses" element={<Courses />} />
+        <Route path="/Community" element={<Community />} />
+        <Route path="/About" element={<AboutUs />} />
+        <Route path="/Contact" element={<ContactUs />} />
+        <Route path="/Signup" element={<Signup />} />
       </Routes>
+      <Footer />
     </AnimatePresence>
   );
 }
