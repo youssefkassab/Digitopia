5aa4676e195f4880b520c64074b3d1276f66f415
youssef kassab <youssefkassab2008@gmail.com>
2025-10-09 11:01:38 +0300

    manfest fix 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -67,7 +67,7 @@ function AppRoutes() {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-                <link rel="manifest" href="/src/assets/manifest.json" />
+                <link rel="manifest" href="/manifest.json" />
                 <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
                 <script type="application/ld+json">
                   {`
